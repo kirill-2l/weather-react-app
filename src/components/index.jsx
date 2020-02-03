9527a31eca4e43f911b1d9bad322104a5dbdbe1d
Petrov Kirill <petrovkirill@Petrovs-iMac.local>
2020-02-03 12:54:12 +0300

    Updates

--- src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export {default as City } from './City';
-export {default as CitiesList } from './CitiesList';
-export {default as Logo } from './Logo';
-export {default as AddCity } from './AddCity';
